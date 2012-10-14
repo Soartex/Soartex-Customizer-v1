f69beea85448677c4bb82e9bc5d5fe005b9f2925
floppygunk <floppygunk@gmail.com>
2012-10-14 17:31:54 +1300

    Fixed config-default.php

--- config-default.php
+++ config-default.php
@@ -3,7 +3,7 @@
  * Copy this file to config.php, putting in the correct values for your
  * MySQL Database.
  */
-define(DBHOST, 'hostname');
-define(DBUSERNAME, 'username');
-define(DBPASSWORD, 'password');
+define('DBHOST', 'hostname');
+define('DBUSERNAME', 'username');
+define('DBPASSWORD', 'password');
 ?>
