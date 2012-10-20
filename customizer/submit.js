9f4ac87151300e70066ffa6d9b992abdd3ef9664
floppygunk <floppygunk@gmail.com>
2012-10-20 23:12:01 +1300

    Added validation for files

--- customizer/submit.js
+++ customizer/submit.js
@@ -11,76 +11,115 @@ function load() {
         $(this).tooltip('destroy');
     });
     
-    var image;
     $('.submit-button').click(function() {
-        if ($('.dropimage').length >= 0) {
+        
+        var image, file, screenshots, is_optional, is_tile, start_x, start_y,
+        start_line, path, name, mod_name, description, creator, validatepath,
+        is_segment
+        ;
+        
+        if ($('.dropimage').length) {
             // Texture data
             image = $('.dropimage').attr('src');
             
             // Array of screenshot data
-            var screenshots = [];
+            screenshots = [];
             $("#snapshot li").each(function() {
                 screenshots.push($(this).children('img').attr('src'));
             });
             
             // Equiv. to true if the 'optional' checkbox is selected
-            var is_optional = $("#is-optional:checked").length
+            is_optional = $("#is-optional:checked").length
             
             // Equiv. to true if the 'tile' checkbox is selected
-            var is_tile = $("#TilesheetCheck:checked").length
+            is_tile = $("#TilesheetCheck:checked").length
             
             // 'start x' and 'start y' data
             if (is_tile) {
-                var start_x = $('#startx').attr('value'),
-                    start_y = $('#starty').attr('value');
+                start_x = $('#startx').attr('value'),
+                start_y = $('#starty').attr('value');
                 var validatedx = /^\+?(0|[1-9]\d*)$/.test(start_x),
-                    validatedy = /^\+?(0|[1-9]\d*)$/.test(start_y);
+                validatedy = /^\+?(0|[1-9]\d*)$/.test(start_y);
                     
-                validate('#startx', validatedx, 'The start x value must be a number', 'top');
+                validate('#startx', validatedx, 'The start x value must be a number', 'bottom');
                 validate('#starty', validatedy, 'The start y value must be a number', 'top');
-            }
-            else {
-                start_x = ""
-                start_y = ""
             }
             
             // Rest of the form
-            var image_path = $('#imagepath').attr('value');
-            var validatepath = /^\/?([\w\d]+\/.)*[\w\d]+\.png$/.test(image_path);
+            path = $('#imagepath').attr('value');
+            validatepath = /^\/?([\w\d]+\/.)*[\w\d]+\.png$/.test(path);
             validate('#imagepath', validatepath, 'The image path must be a valid path ending in .png');
             
-            var texture_name = $('#texturename').attr('value');
-            validate('#texturename', texture_name, 'Please enter in a texture name');
+            name = $('#texturename').attr('value');
+            validate('#texturename', name, 'Please enter in a texture name');
             
-            var mod_name = $('#image-modname').attr('value');
+            mod_name = $('#image-modname').attr('value');
             validate('#image-modname', mod_name, 'Please enter in a mod name');
             
-            var description = $('#edit-description').attr('value');
-            var creator = $('creator');
-            
-            // The data that will be submitted 
-            data = {
-                image : image,
-                screenshots: JSON.stringify(screenshots),
-                image_path: image_path,
-                is_optional: is_optional,
-                is_tile: is_tile,
-                start_x: start_x,
-                start_y: start_y,
-                texture_name: texture_name,
-                mod_name: mod_name,
-                description: description,
-                creator: creator
-            };
-        //postIt('submit.php', data);
-            
-            
+            description = $('#edit-description').attr('value');
+            creator = $('creator');
+           
         }
+        
+        // If the file is text
+        else if ($('.dropfile').length) {
+            // Texture data
+            file = $('.dropfile').attr('src');
+            
+            // Array of screenshot data
+            screenshots = [];
+            $("#snapshot li").each(function() {
+                screenshots.push($(this).children('img').attr('src'));
+            });
+            
+            // Equiv. to true if the 'optional' checkbox is selected
+            is_optional = $("#file-optional:checked").length
+            
+            // Equiv. to true if the 'tile' checkbox is selected
+            is_segment = $("#fileline-check:checked").length
+            
+            // 'start x' and 'start y' data
+            if (is_segment) {
+                start_line = $('#startx').attr('value');
+                var validatedline = /^\+?(0|[1-9]\d*)$/.test(start_line);
+                validate('#startline', validatedline, 'The start line value must be a number');
+            }
+            
+            // Rest of the form
+            path = $('#file-path').attr('value');
+            validatepath = /^\/?([\w\d]+\/.)*[\w\d]+\.[\w\d]*$/.test(path);
+            validate('#file-path', validatepath, 'The file path must be a valid path');
+            
+            name = $('#file-name').attr('value');
+            validate('#file-name', name, 'Please enter in a file name');
+            
+            mod_name = $('#file-mod-name').attr('value');
+            validate('#file-mod-name', mod_name, 'Please enter in a mod name');
+            
+            description = $('#edit-description').attr('value');
+            creator = $('creator');
+        }
+        
+        // The data that will be submitted 
+        var data = {
+            image : image,
+            file : file,
+            screenshots: JSON.stringify(screenshots),
+            path: path,
+            is_optional: is_optional,
+            is_tile: is_tile,
+            start_x: start_x,
+            start_y: start_y,
+            start_line : start_line,
+            name: name,
+            description: description,
+            creator: creator
+        };
+    //postIt('submit.php', data);
     });
 }
 
 function validate(element, bool, text, dir) {
-    console.log(1);
     if (!bool) {
         errorTip(element, text, dir)
     }
@@ -89,12 +128,12 @@ function validate(element, bool, text, dir) {
 function errorTip(element, text, dir) {
     var direction = dir || 'right'
     $(element)
-        .tooltip({
-            title: '<i class="icon-warning-sign icon-white"></i> ' + text,
-            trigger: 'manual',
-            placement: direction
-        })
-        .tooltip('show')
+    .tooltip({
+        title: '<i class="icon-warning-sign icon-white"></i> ' + text,
+        trigger: 'manual',
+        placement: direction
+    })
+    .tooltip('show')
 }
 
 function postIt(url, data){
