e8d41d3d53a13a4341ae6a4ff5ee0f0363f9341a
REDX36 <redx36@hotmail.com>
2012-12-18 15:07:07 -0500

    Basic index page.

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+	
+		<title>Soartex Customizer</title>
+		<meta charset="utf-8"/>
+		
+		<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css" />
+		
+		<style type="text/css">
+	      
+	    	body {
+	        	padding-top: 60px;
+	        	padding-bottom: 40px;
+	   		}
+	      
+	    </style>
+		
+	</head>
+
+	<body>
+	
+		<div class="container">
+	
+			<h1>Soartex Customizer</h1>
+		
+		</div>
+	
+	</body>
+
+</html>
