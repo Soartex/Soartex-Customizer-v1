28cca9e4deb4cb3463ae65fdf4292385acf6af5a
REDX36 <redx36@hotmail.com>
2012-12-18 18:37:46 -0500

    Footer formatting change.

--- index.php
+++ index.php
@@ -3,39 +3,41 @@
 <html>
 
 	<head>
-  
+
 		<title>Soartex Customizer</title>
 		<meta charset="utf-8"/>
-    
+
 		<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css" />
 		<link rel="stylesheet" type="text/css" href="assets/css/bootstrap-responsive.css">
-    
+
 		<link rel="stylesheet" type="text/css" href="assets/css/global.css" />
-    
-    </head>
+
+	</head>
 
 	<body>
-  
+
 		<div class="container">
-  
+
 			<div class="masthead">
-            
+
 				<h3 class="muted">Soartex Customizer</h3>
-          
+
 			</div>
-  
+
 			<hr/>
-  
+
 			Content goes here!
 
+			<hr/>
+
 			<div class="footer">
-      
+
 				<p>&copy; Soartex Fanver 2012</p>
-            
+
 			</div>
-    
+
 		</div>
-  
+
 	</body>
 
 </html>
