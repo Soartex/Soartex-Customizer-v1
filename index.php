229b674edfd7b9210dfed37c8e5d9f23962ea1c8
REDX36 <redx36@hotmail.com>
2012-12-19 12:56:57 -0500

    Using LESS, not working.

--- index.php
+++ index.php
@@ -7,10 +7,13 @@
 		<title>Soartex Customizer</title>
 		<meta charset="utf-8"/>
 
-		<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css" />
-		<link rel="stylesheet" type="text/css" href="assets/css/bootstrap-responsive.css">
+		
+		<link rel="stylesheet/less" href="assets/less/bootstrap/bootstrap.less">
+		<script src="assets/js/less-1.3.1.min.js"></script>
+
 
 		<link rel="stylesheet" type="text/css" href="assets/css/global.css" />
+
 
 	</head>
 
