c0615a3dc4280a327926b82f1174d4a0d0085038
goldbattle <patfgeneva@gmail.com>
2013-01-24 23:27:09 -0500

    Made new footer. Added Header. Added Tabs. Don't know how to do the java script for the tabs, But they are added. Added internal bootstrap

--- index.php
+++ index.php
@@ -3,48 +3,84 @@
 <html>
 
 	<head>
-
 		<title>Soartex Customizer</title>
 		<meta charset="utf-8"/>
-
-		<link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.2.2/css/bootstrap-combined.min.css" rel="stylesheet">
-
+		<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css" />
 		<link rel="stylesheet" type="text/css" href="assets/css/global.css" />
-
+		<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
 	</head>
 
 	<body>
-
 		<div class="container">
-
-			<div class="masthead">
-
-				<ul class="nav nav-pills pull-right">
-
-					<li class="active"><a href="#">Home</a></li>
-					<li><a href="admin.php">Admin</a></li>
-
-				</ul>
-
-				<h3 class="muted">Soartex Customizer</h3>
-
+			<div class="page-header">
+				<h1>Soartex Customizer <small>Make our pack, your pack!</small></h1>
 			</div>
-
-			<hr/>
-
-			Content goes here!
-
-			<hr/>
-
+			<section>
+				Here is here I would write a amazing blurb about how this site was created so that the user can make a awsome soartex pack.
+				I would then go on to talk about other things. Maybe about how we made it and how a wonderfull community we are and
+				how you the view should come and participate in it. Just some ideas. I don't know how else to make this any longer but here it is.
+				Content goes here!
+			</section>
+			<!--tabbed area-->
+			<section  style="margin-top: 60px;">
+				<div class="tabbable">
+					<ul id="tab" class="nav nav-tabs">
+						<li class="active">
+							<a data-toggle="tab" href="#tab1">Soartex Vanilla</a>
+						</li>
+						<li>
+							<a data-toggle="tab" href="#tab2">Soartex Modded</a>
+						</li>
+						<li>
+							<a data-toggle="tab" href="#tab3">Soartex Seasonal</a>
+						</li>
+					</ul>
+					<!--tab content-->
+					<div class="tab-content" style="overflow: visible;">
+						<div class="tab-pane active" id="tab1">
+							<div class="row"></div>
+							<div class="row active">
+								<div class="span6">
+									Vanilla Content
+								</div>
+							</div>
+						</div>
+						<div class="tab-pane" id="tab2">
+							<div class="row"></div>
+							<div class="row">
+								<div class="span6">
+									Modded Content
+								</div>
+							</div>
+						</div>
+						<div class="tab-pane" id="tab3">
+							<div class="row"></div>
+							<div class="row">
+								<div class="span6">
+									Seasonal Content
+								</div>
+							</div>
+						</div>
+					</div>
+					<!--end of tab content-->
+				</div>
+			</section>
+			<!--footer-->
 			<footer>
-
-				<h4 style="float: left" class="muted">&copy; Soartex Fanver 2012</h4>
-				<h4 style="float: right"><a href="http://www.soartex.net/" title="Soartex Site">Go to the Soartex Site!</a></h4>
-
+				<hr>
+				<ul class="nav nav-pills">
+					<li class="pull-left">
+						<a href="http://soartex.net/">© Soartex 2012-2013</a>
+					</li>
+					<li class="pull-right">
+						<a href="admin.php">Admin</a>
+					</li>
+					<li class="pull-right active">
+						<a href="#">User</a>
+					</li>
+				</ul>
 			</footer>
-
+			<!--end of footer-->
 		</div>
-
 	</body>
-
 </html>
