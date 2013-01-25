263c186a5f245076e70d3ae74ae82dca38079db5
goldbattle <patfgeneva@gmail.com>
2013-01-25 17:59:26 -0500

    Trying out a design for the main page.
    http://i.imgur.com/xtCrbHo.jpg

--- index.php
+++ index.php
@@ -3,255 +3,208 @@
 <html>
 
 	<head>
-		
+
 		<title>Soartex Customizer</title>
 		<meta charset="UTF-8"/>
-		
+
 		<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css" />
 		<link rel="stylesheet" type="text/css" href="assets/css/global.css" />
-		
+
 		<script type="text/javascript" src="http://code.jquery.com/jquery.min.js"></script>
 		<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
-		
+
 	</head>
 
 	<body>
-		
+
 		<div class="container">
-			
 			<div class="page-header">
-				
 				<h1>Soartex Customizer <small>Make our pack, your pack!</small></h1>
-				
 			</div>
-			
+
 			<section id="intro">
-				
 				Here is here I would write a amazing blurb about how this site was created so that the user can make a awsome soartex pack. I would then go on to talk about other things. Maybe about how we made it and how a wonderfull community we are and how you the view should come and participate in it. Just some ideas. I don't know how else to make this any longer but here it is. Content goes here!
-			
 			</section>
 
 			<section id="main">
-				
 				<div class="tabbable">
-					
 					<ul id="tab" class="nav nav-tabs">
-						
 						<li class="active">
-							
 							<a data-toggle="tab" href="#tab1">Soartex Vanilla</a>
-							
 						</li>
-						
+
 						<li>
-							
 							<a data-toggle="tab" href="#tab2">Soartex Modded</a>
-							
 						</li>
-						
+
 						<li>
-							
 							<a data-toggle="tab" href="#tab3">Soartex Seasonal</a>
-							
 						</li>
-						
 					</ul>
 
 					<div class="tab-content" style="overflow: visible;">
-						
 						<div class="tab-pane active" id="tab1" style="margin-top: 70px;">
-							
 							<div class="tabbable tabs-left">
-
 								<ul class="nav nav-tabs">
-
 									<li class="active">
 										<a data-toggle="tab" href="#stab1">Armor</a>
-
 									</li>
-									
 									<li>
-										
 										<a data-toggle="tab" href="#stab2">Art</a>
-										
 									</li>
-									
 									<li>
-										
-										<a data-toggle="tab" href="#stab3">Food</a>
-										
+										<a data-toggle="tab" href="#stab3">Items</a>
 									</li>
-									
-									<li>
-										
-										<a data-toggle="tab" href="#stab4">Guis</a>
-										
-									</li>
-									
-									<li>
-										
-										<a data-toggle="tab" href="#stab5">Icons</a>
-										
-									</li>
-
-									<li>
-
-										<a data-toggle="tab" href="#stab6">Items</a>
-
-									</li>
-									
-									<li>
-										
-										<a data-toggle="tab" href="#stab7">Misc</a>
-										
-									</li>
-									
-									<li>
-										
-										<a data-toggle="tab" href="#stab8">Particals</a>
-										
-									</li>
-									
-									<li>
-										
-										<a data-toggle="tab" href="#stab9">Plants and Shrubbery</a>
-										
-									</li>
-									
 								</ul>
 
 								<div class="tab-content">
 
 									<div class="tab-pane active" id="stab1">
-
 										Yo! Picture of Armor here. Select your favorite
-										
 									</div>
-									
 									<div class="tab-pane" id="stab2">
-										
 										More pictures of Art
-										
 									</div>
-									
 									<div class="tab-pane" id="stab3">
-										
-										More pictures of Food
-										
-									</div>
-									
-									<div class="tab-pane" id="stab4">
-
-										More pictures of Guis
-
-									</div>
-
-									<div class="tab-pane" id="stab6">
-
+										<!--brewing-->
+										<legend>Brewing Item</legend>
 										<ul class="thumbnails">
-
 											<li>
-
 												<div class="thumbnail">
-
 													<img src="assets/img/brewingitem.png">
-
 													<div class="caption">
-
-														<h5>Brewing Item</h5>
-
 														<p>
-
 															- GoldBattle
-
 														</p>
-
+														<button class="btn btn-block btn-primary disabled" type="button">
+															Select
+														</button>
 													</div>
-
 												</div>
-
 											</li>
-
 											<li>
-
 												<div class="thumbnail">
-
-													<img src="assets/img/cauldronitem.png">
-
+													<img src="http://placehold.it/64x64">
 													<div class="caption">
-
-														<h5>Cauldron Item</h5>
-
 														<p>
-
-															- GoldBattle
-
+															- Someone Else
 														</p>
-
+														<button class="btn btn-block" type="button">
+															Select
+														</button>
 													</div>
-
 												</div>
-
 											</li>
-
+											<li>
+												<div class="thumbnail">
+													<img src="http://placehold.it/64x64">
+													<div class="caption">
+														<p>
+															- Someone
+														</p>
+														<button class="btn btn-block" type="button">
+															Select
+														</button>
+													</div>
+												</div>
+											</li>
 										</ul>
 
-									</div>
-									
-								</div>
-								
-							</div>
-							
-						</div>
-						
-						<div class="tab-pane" id="tab2">
-							
-							Modded Content
-							
-						</div>
-						
-						<div class="tab-pane" id="tab3">
-							
-							Seasonal Content
-							
-						</div>
-						
-					</div>
+										<!--cauldren-->
+										<legend>Cauldron Item</legend>
+										<ul class="thumbnails">
+											<li>
+												<div class="thumbnail">
+													<img src="assets/img/cauldronitem.png">
+													<div class="caption">
+														<p>
+															- GoldBattle
+														</p>
+														<button class="btn btn-block btn-primary disabled" type="button">
+															Select
+														</button>
+													</div>
+												</div>
+											</li>
+											<li>
+												<div class="thumbnail">
+													<img src="http://placehold.it/64x64">
+													<div class="caption">
+														<p>
+															- Someone
+														</p>
+														<button class="btn btn-block" type="button">
+															Select
+														</button>
+													</div>
+												</div>
+											</li>
+										</ul>
 
+										<!--Sword-->
+										<legend>Sword Item</legend>
+										<ul class="thumbnails">
+											<li>
+												<div class="thumbnail">
+													<img src="http://placehold.it/64x64">
+													<div class="caption">
+														<p>
+															- Someone
+														</p>
+														<button class="btn btn-block btn-primary disabled" type="button">
+															Select
+														</button>
+													</div>
+												</div>
+											</li>
+											<li>
+												<div class="thumbnail">
+													<img src="http://placehold.it/64x64">
+													<div class="caption">
+														<p>
+															- Someone
+														</p>
+														<button class="btn btn-block" type="button">
+															Select
+														</button>
+													</div>
+												</div>
+											</li>
+										</ul>
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<div class="tab-pane" id="tab2">
+							Modded Content
+						</div>
+
+						<div class="tab-pane" id="tab3">
+							Seasonal Content
+						</div>
+
+					</div>
 				</div>
-				
 			</section>
 
 			<footer>
-				
 				<hr/>
-				
 				<ul class="nav nav-pills">
-					
 					<li class="pull-left">
-						
 						<a href="http://soartex.net/">© Soartex 2012-2013</a>
-						
 					</li>
-					
 					<li class="pull-right">
-						
 						<a href="admin.php">Admin</a>
-						
 					</li>
-					
 					<li class="pull-right active">
-						
 						<a href="#">User</a>
-						
 					</li>
-					
 				</ul>
-				
 			</footer>
 
 		</div>
-		
+
 	</body>
-	
+
 </html>
