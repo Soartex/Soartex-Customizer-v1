09a583fee5a02d13f8c6910b0af7c4be902a8a96
floppygunk <floppygunk@gmail.com>
2013-01-26 22:43:45 +1300

    Formatting

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,20 +1,20 @@
 function mixin(destination, source) {
-    for (var k in source) {
-        if (source.hasOwnProperty(k)) {
-            destination[k] = source[k];
-        }
-    }
-    return destination;
+	for (var k in source) {
+		if (source.hasOwnProperty(k)) {
+			destination[k] = source[k];
+		}
+	}
+	return destination;
 }
 
 function extend(subClass, superClass) {
-    var F = function() {};
-    F.prototype = superClass.prototype;
-    subClass.prototype = new F();
-    subClass.prototype.constructor = subClass;
+	var F = function () { };
+	F.prototype = superClass.prototype;
+	subClass.prototype = new F();
+	subClass.prototype.constructor = subClass;
 
-    subClass.superclass = superClass.prototype;
-    if(superClass.prototype.constructor == Object.prototype.constructor) {
-        superClass.prototype.constructor = superClass;
-    }
+	subClass.superclass = superClass.prototype;
+	if (superClass.prototype.constructor == Object.prototype.constructor) {
+		superClass.prototype.constructor = superClass;
+	}
 }
