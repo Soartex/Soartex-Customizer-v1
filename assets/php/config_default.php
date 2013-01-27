dd1ef755ec09af6b08effba93e70006b9e96ac1b
floppygunk <floppygunk@gmail.com>
2013-01-28 12:11:34 +1300

    Added config_default for storing private data

---
+++ assets/php/config_default.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copy this file to config.php, and enter in the database information
+ */
+
+define("DBHOST", "[Host]");
+define("DBUSERNAME", "[Username]");
+define("DBPASSWORD", "[Password]");
+define("DBDATABASE", "[Database]");
