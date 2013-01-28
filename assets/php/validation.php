7e936aa25279d28e3f76f6f0330d1c562aa86128
floppygunk <floppygunk@gmail.com>
2013-01-28 14:58:36 +1300

    Added tools for validation

---
+++ assets/php/validation.php
@@ -0,0 +1,9 @@
+<?php
+
+function is_valid_int ($string) {
+	return preg_match ('/^\\d+\\z/', $string);
+}
+
+function is_valid_string ($string, $maxLength=-1) {
+	return (is_string($string) && ($maxLength >= 0 || strlen($string) <= $maxLength));
+}
