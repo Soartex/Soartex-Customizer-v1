38113f37847e00aa17e7aeb11956b4dd217e7420
floppygunk <floppygunk@gmail.com>
2013-01-28 22:57:46 +1300

    Merge fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 	for (var z = 0; z < picGroups.length-1; z++){
 		groupArray[z] = new TextureGroup({
 			id : z+1,
-			groupName : 'Test Texture Group ' +(z+1),
+			groupName : 'Test category ' +(z+1),
 			textures : [],
 			exportData : '{"exportPath":"gui/items.png", "exportX":768, "exportY":256}'
 		});
@@ -13,7 +13,7 @@ $(document).ready(function() {
 		for (var i = picGroups[z]; i < picGroups[z+1]; i++) {
 			var data = {
 				id : i+1,
-				category : groupArray[z],
+				group : groupArray[z],
 				presets : [],
 				creator : "Texture " + (i-picGroups[z]+1),
 				dateAdded : new Date(),
@@ -24,30 +24,10 @@ $(document).ready(function() {
 			tempArray.push(new TextureOption(data));
 		}
 		groupArray[z].setTextures(tempArray);
-	 }
-
-	var temp = new TextureOption({
-		id : 0,
-		categoryName : 'Test category',
-		textures : [],
-		exportData : '{"exportPath":"gui/items.png", "exportX":768, "exportY":256}'
-	});
-
-	tempArray = []
-	for (var i = 0; i < 50; i++) {
-		var data = {
-			id : i+1,
-			category : temp,
-			presets : [],
-			creator : "Texture " + i,
-			dateAdded : new Date(),
-			screenshots : [],
-			exportData : '{}',
-			path : "temp"
-		};
-		tempArray[i] = new TextureOption(data);
 	}
-	temp.setTextures(tempArray);
 
-	$('#stab3').append(temp.getHtml());
+	for(var j=0; j<groupArray.length; j++){
+		$('#stab1').append(groupArray[j].getHtml());
+	}
+
 });
