1c574a1e9a6059265371595667dfeda04cbf1841
floppygunk <floppygunk@gmail.com>
2013-02-24 19:12:00 +1300

    Comments

--- assets/php/config_default.php
+++ assets/php/config_default.php
@@ -3,14 +3,13 @@
     // Rename this file to config.php, and enter in the details.
     
     // Database connection.
-    
     $db_host = "[host]";
     $db_username = "[user]";
     $db_password = "[password]";
     $db_database = "[database]";
     
     // Branding
-    
+	// May be used if the customizer is expanded to work with custom texture packs
     $name = "[name]";
     $into = "[intro]";
 
