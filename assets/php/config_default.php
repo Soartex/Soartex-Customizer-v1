cedecf0181546f01cfff3e8b6bd2ea2d720b3fe1
floppygunk <floppygunk@gmail.com>
2013-03-12 20:02:21 +1300

    Updated variables

--- assets/php/config_default.php
+++ assets/php/config_default.php
@@ -1,16 +1,14 @@
 <?php
 
-    // Rename this file to config.php, and enter in the details.
-    
-    // Database connection.
-    $db_host = "[host]";
-    $db_username = "[user]";
-    $db_password = "[password]";
-    $db_database = "[database]";
-    
-    // Branding
-	// May be used if the customizer is expanded to work with custom texture packs
-    $name = "[name]";
-    $into = "[intro]";
+// Rename this file to config.php, and enter in the details.
 
-?>
+// Database connection.
+define("DBHOST", "[host]");
+define("DBUSERNAME", "[user]");
+define("DBPASSWORD", "[password]");
+define("DBDATABASE", "[database]");
+
+// Branding
+// May be used if the customizer is expanded to work with custom texture packs
+$name = "[name]";
+$into = "[intro]";
