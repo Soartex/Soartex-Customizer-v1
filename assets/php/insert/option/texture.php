8fe9818f08315cbad2df6b661b4860ae41da9eeb
floppygunk <floppygunk@gmail.com>
2013-06-22 12:05:10 +1200

    Added password and removed presets

--- assets/php/insert/option/texture.php
+++ assets/php/insert/option/texture.php
@@ -1,20 +1,23 @@
 <?php
 
+	header("Access-Control-Allow-Origin: *"); // NOTE: MUST BE CHANGED TO "http://soartex.net" BEFORE RELEASE
+
 	// Connect to database; include validation scripts.
 
-	require_once("../db_connect.php");
-	include_once("../validation.php");
-	
+	require_once("../../db_connect.php");
+	include_once("../../validation.php");
+	require_once("../../config.php");
+
+    // Password must be correct
+
+	assert($_POST["password"] == ADMINPASSWORD);
+
 	// Compile the texture record information.
-	
+
 	$creator = $mysqli->real_escape_string($_POST["creator"]);
 	assert(is_valid_string($creator, 25));
 	
 	$info = $mysqli->real_escape_string($_POST["info"]);
-	assert(is_valid_string($info, 255));
-	
-	$preset = $_POST["preset"];
-	assert(is_valid_int($preset));
 	
 	$group = $_POST["group"];
 	assert(is_valid_int($group));
@@ -22,8 +25,8 @@
 	$export_data = $mysqli->real_escape_string($_POST["export_data"]);
 	assert(is_valid_string($export_data));
 	
-	$mysqli->query("INSERT INTO Textures (name, creator, info, preset, group_id, date_added, export_data)
-					VALUES ('$name', '$creator', '$info', $preset, $group, '$export_data')");
+	$mysqli->query("INSERT INTO Textures (name, creator, info, group_id, date_added, export_data)
+					VALUES ('$name', '$creator', '$info', $group, '$export_data')");
 
 	$lastID = $mysqli->insert_id;
 	
