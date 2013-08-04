7f667284f375ed0c867cd5d941676a87160fe09b
floppygunk <floppygunk@gmail.com>
2013-08-04 12:32:22 +1200

    Added files for deleting textures

---
+++ assets/php/delete/category.php
@@ -0,0 +1,20 @@
+<?php
+
+header("Access-Control-Allow-Origin: *"); // NOTE: MUST BE CHANGED TO "http://soartex.net" BEFORE RELEASE
+
+// Connect to database; include validation scripts.
+
+require_once("../db_connect.php");
+include_once("../validation.php");
+require_once("../config.php");
+
+$valid = true;
+
+$valid = $valid && $_POST["password"] == ADMINPASSWORD;
+
+$id = $_POST["id"];
+$valid = $valid && is_valid_int($id);
+
+if ($valid) {
+	$result = $mysqli->query("DELETE FROM Categories WHERE category_id = $id");
+}
